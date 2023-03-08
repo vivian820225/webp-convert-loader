9596575b471258809d858fef11b2b57d72bc7b98
VivianChiang <vivian820225@gmail.com>
2023-03-08 15:22:57 +0800

    [Add] webp-convert-loader

---
+++ README.md
@@ -0,0 +1,62 @@
+# webp-convert-loader
+A webpack loader that can convert small images into base64 format and generate new webp images with the hash of the source file name. It also preserves the source image.
+## Features
+	1. In development mode, a webp image with the same name will be generated in the cache.
+  2. Supports passing the limit parameter, which limits images under 10k to be converted to base64, while images above that size will be generated as webp images with the same name.
+## Installation
+```
+npm install --save-dev webp-convert-loader
+```
+## Usage
+1. webpack / vue-cli2.0 configure as follows:
+```
+{
+  test: /\.(png|jpe?g)(\?.*)?$/,
+  use: [{
+    loader: "webp-convert-loader",
+    options: {
+      limit: 8192,
+      quality: 90,
+      name: 'static/img/[name].[hash:8].[ext]'
+    }
+  }]
+}
+```
+2. vue-cli3.0 configure as follows:
+```
+module.exports = {
+  chainWebpack: config => {
+    const imageRule = config.module.rule('images')
+    imageRule.uses.clear()
+    config.module.rule('webp')
+      .test(/\.(jpe?g|png)$/i)
+      .use('webp-convert-loader')
+      .loader('webp-convert-loader')
+      .options({
+        limit: 8192,
+        quality: 80,
+        name: `static/img/[name].[hash:8].[ext]`
+      })
+      .end()
+  }
+}
+```
+## API
+### limit
+Type: `string|number`  
+Default: `10240`  
+Converts images under 10k to base64 by default.  
+### quality  
+Type: `string|number`  
+Default: `80`  
+The quality of the webp image generated from png or jpg.  
+### name
+Type: `string`  
+Default: `[hash].[ext]`  
+The filename and relative path of the generated webp image.  
+### 其他API
+Other parameters can refer to [imagemin-webp](https://github.com/imagemin/imagemin-webp).  
+
+## Tips
+1. If cwebp-bin throws an error, check whether there is a vendor folder under cwebp-bin in node_modules. If not, you need to reinstall this dependency package.
+
---
+++ index.js
@@ -0,0 +1,90 @@
+/**
+ * Options for image converter.
+ * @typedef {Object} converterOptions
+ * @property {string} name - The name of the compressed image file. If not passed, the hash will be used by default.
+ * @property {string} preset - The type of file, which can be default, photo, picture, drawing, icon, or text.
+ * @property {number} quality - The quality of the compressed image.
+ * @property {number} alphaQuality - Set the transparency compression quality between 0 and 100.
+ * @property {number} method - Specify the compression method to use between 0 (fastest) and 6 (slowest). This parameter controls the trade-off between encoding speed, file size, and quality.
+ * @property {number} sns - Set the amplitude of spatial noise shaping between 0 and 100.
+ * @property {boolean} autoFilter - Set the strength of block filtering between 0 (off) and 100.
+ * @property {number} sharpness - Set the sharpness of the filter between 0 (sharpest) and 7 (least sharp).
+ * @property {boolean} lossless - Whether to use lossless encoding for the image.
+ */
+
+var imagemin = require("imagemin");
+var imageminWebp = require("imagemin-webp");
+var loaderUtils = require("loader-utils");
+var mime = require("mime");
+
+module.exports = function (content) {
+  this.cacheable && this.cacheable();
+  if (!this.emitFile) {
+    throw new Error("emitFile is required from module system");
+  }
+  var callback = this.async();
+  var called = false;
+  // Get configuration parameters
+  var query = loaderUtils.getOptions(this);
+  // Generate hash
+  var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
+    content: content,
+    regExp: query.regExp,
+  });
+
+  // Generate webp url
+  var webpUrl = url.substring(0, url.lastIndexOf(".")) + ".webp";
+  // Set default value
+  let limit = 10 * 1024;
+  if (query.limit) {
+    limit = parseInt(query.limit, 10);
+  }
+  // Get the file type
+  var mimetype = query.mimetype || query.minetype || mime.getType(this.resourcePath);
+  // If less than the default 10k or the custom range, output base64 directly
+  if (limit <= 0 || content.length < limit) {
+    return callback(null, "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64")));
+  }
+
+  // Define the parameters for the output webp file
+  var options = {
+    preset: query.preset || "default",
+    quality: query.quality || 80,
+    alphaQuality: query.alphaQuality || 100,
+    method: query.method || 1,
+    sns: query.sns || 80,
+    autoFilter: query.autoFilter || false,
+    sharpness: query.sharpness || 0,
+    lossless: query.lossless || false,
+    bypassOnDebug: query.bypassOnDebug || false,
+  };
+
+  if (query.size) {
+    options.size = query.size; // Set target size (in bytes)
+  }
+
+  if (query.filter) {
+    options.filter = query.filter;
+  }
+
+  // Debugging
+  if (this.debug === true && options.bypassOnDebug === true) {
+    callback(null, "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";");
+  } else {
+  // image compression
+  imagemin
+    .buffer(content, { plugins: [imageminWebp(options)] })
+    .then((file) => {
+      // Output the source file
+      this.emitFile(url, content);
+      // Output the webp file
+      this.emitFile(webpUrl, file);
+      // Default to display the original image
+      callback(null, "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";");
+    }).catch((err) => {
+      callback(err);
+    });
+  }
+};
+
+module.exports.raw = true;
